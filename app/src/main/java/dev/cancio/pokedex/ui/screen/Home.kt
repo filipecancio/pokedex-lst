19aa51ebb576c624271234183392912aa109e8fa
cancio <filipe.cancio@gmail.com>
2022-05-14 00:30:05 -0300

    :tada: Criando telas

---
+++ app/src/main/java/dev/cancio/pokedex/ui/screen/Home.kt
@@ -0,0 +1,8 @@
+package dev.cancio.pokedex.ui.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+
+}
---
+++ app/src/main/java/dev/cancio/pokedex/ui/screen/PokeApp.kt
@@ -0,0 +1,24 @@
+package dev.cancio.pokedex.ui.screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import dev.cancio.pokedex.ui.theme.PokedexTheme
+
+@Composable
+fun PokeApp() {
+    PokedexTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            PokedexTheme {
+                Text(text = "Pokedex!")
+            }
+        }
+    }
+}
