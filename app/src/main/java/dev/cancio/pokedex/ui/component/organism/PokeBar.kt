9e95d3cce5c02a2c04333256046a0b038c28323b
cancio <filipe.cancio@gmail.com>
2022-05-14 01:08:40 -0300

    :construction: Criando bottom bar

--- app/src/main/java/dev/cancio/pokedex/ui/component/organism/PokeBar.kt
+++ app/src/main/java/dev/cancio/pokedex/ui/component/organism/PokeBar.kt
@@ -10,8 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import dev.cancio.pokedex.ui.screen.HomeScreen
 import dev.cancio.pokedex.ui.screen.LikeScreen
+import dev.cancio.pokedex.ui.component.organism.BottomNavItem.Likes
+import dev.cancio.pokedex.ui.component.organism.BottomNavItem.Home
 
 @Composable
 fun PokeBar(navController: NavController, itemList: List<BottomNavItem>)  {
@@ -39,6 +44,13 @@ fun PokeBar(navController: NavController, itemList: List<BottomNavItem>)  {
     }
 }
 
+@Composable
+fun MainNavigation(navController: NavHostController, itemList: List<BottomNavItem>) {
+    NavHost(navController = navController, startDestination = itemList.first().route) {
+        itemList.forEach { item -> composable(item.route) { item.screen.invoke() } }
+    }
+}
+
 sealed class BottomNavItem(
     val route: String,
     val icon: ImageVector,
@@ -46,4 +58,8 @@ sealed class BottomNavItem(
     val screen: (@Composable () -> Unit)) {
     object Home : BottomNavItem("home", Icons.Filled.Home, "Home", { HomeScreen() })
     object Likes : BottomNavItem("likes", Icons.Filled.Favorite, "Likes", { LikeScreen() })
+}
+
+sealed class PokedexRoutes(val itemList: List<BottomNavItem>){
+    object MainRoute: PokedexRoutes(listOf(Home, Likes))
 }
--- app/src/main/java/dev/cancio/pokedex/ui/screen/PokeApp.kt
+++ app/src/main/java/dev/cancio/pokedex/ui/screen/PokeApp.kt
@@ -1,23 +1,34 @@
+@file:Suppress("OPT_IN_IS_NOT_ENABLED")
+
 package dev.cancio.pokedex.ui.screen
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import dev.cancio.pokedex.ui.component.organism.MainNavigation
+import dev.cancio.pokedex.ui.component.organism.PokeBar
+import dev.cancio.pokedex.ui.component.organism.PokedexRoutes.MainRoute
 import dev.cancio.pokedex.ui.theme.PokedexTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PokeApp() {
     PokedexTheme {
         // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+            color = MaterialTheme.colorScheme.background,
         ) {
-            PokedexTheme {
-                HomeScreen()
+            val navController = rememberNavController()
+            Scaffold(
+                bottomBar = { PokeBar(navController, MainRoute.itemList )}
+            ) {
+                MainNavigation(navController, MainRoute.itemList )
             }
         }
     }
